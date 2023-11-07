3d5d5ff4dca9c2eb314bd36e626976acb9b911d3
Amr Saber <amrsaber@amazon.com>
2023-11-07 10:12:39 +0000

    add install and uninstall scripts

---
+++ install.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/env bash
+
+# Setup tmux config
+mkdir -p ~/.config/tmux
+ln "$(pwd)/tmux/tmux.conf" "$HOME/.config/tmux/tmux.conf"
+
---
+++ uninstall.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env bash
+
+# Uninstall tmux config
+rm ~/.config/tmux/tmux.conf
+
