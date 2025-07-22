f9fed1474b191077bce8200c68a39b1c5437403c
Amr Saber <amrsaber@amazon.com>
2025-07-22 22:53:30 +0100

    nvim: faster smear

--- configs/nvim/.config/nvim/lua/plugins/smear-cursor.lua
+++ configs/nvim/.config/nvim/lua/plugins/smear-cursor.lua
@@ -1,4 +1,15 @@
 return {
   "sphamba/smear-cursor.nvim",
-  opts = {},
+  opts = {
+    -- Suggested options for faster smear
+    stiffness = 0.8,
+    trailing_stiffness = 0.5,
+    stiffness_insert_mode = 0.7,
+    trailing_stiffness_insert_mode = 0.7,
+    damping = 0.8,
+    damping_insert_mode = 0.8,
+    distance_stop_animating = 0.5,
+
+    time_interval = 7,
+  },
 }
