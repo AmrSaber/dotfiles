0378e1475f4c56628c7b95103078d200cb2b9259
Amr Saber <amr.m.saber.mail@gmail.com>
2025-08-10 01:36:55 +0100

    nvim: Optimize tsserver

--- configs/nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ configs/nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -1,6 +1,22 @@
 return {
   "neovim/nvim-lspconfig",
   opts = {
+    servers = {
+
+      -- Optimize tsserver a bit
+      tsserver = {
+        flags = { debounce_text_changes = 300 },
+        handlers = { ["textDocument/semanticTokens/full"] = function() end },
+        init_options = {
+          maxTsServerMemory = 4096,
+          preferences = {
+            disableSuggestions = true,
+            disableAutomaticTypeAcquisition = true,
+          },
+        },
+      },
+    },
+
     inlay_hints = {
       enabled = false,
     },
