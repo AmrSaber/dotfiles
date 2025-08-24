f566c4186cdba44e3b05a607010d86b7a6a468ea
Amr Saber <amr.m.saber.mail@gmail.com>
2025-08-24 07:35:42 +0100

    update init-termux

--- init-termux.sh
+++ init-termux.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/bash
 
-nala install curl git openssh
-nala install neovim just zellij stow gum skate jq # Core tools
-nala install bat fd ripgrep eza zoxide            # Modern alternatives
+nala install curl git openssh zsh neovim just zellij stow gum skate jq # Core tools
+nala install bat fd ripgrep eza zoxide yazi                            # Modern alternatives
+
+# Install grun, glibc-wrapper. This must be on 2 steps
+nala install glibc-repo
+nala install glibc-runner
 
 # Install oh-my-zsh
 [[ ! -d ~/.oh-my-zsh ]] && sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
