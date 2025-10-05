0a179171dbcbfd82a6fd08ad11a41ee9fb02c6de
Amr Saber <amr.m.saber.mail@gmail.com>
2025-10-05 20:12:49 +0000

    nvim: update popups color

--- configs/nvim/.config/nvim/lua/config/options.lua
+++ configs/nvim/.config/nvim/lua/config/options.lua
@@ -10,10 +10,6 @@ if os.getenv("SSH_CLIENT") or os.getenv("SSH_TTY") then
       ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
       ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
     },
-    paste = {
-      ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
-      ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
-    },
   }
 end
 
@@ -24,3 +20,14 @@ vim.opt.conceallevel = 0
 
 -- Disable trouble lualine. No need for noisy components in lualine
 vim.g.trouble_lualine = false
+
+-- Override colors for popups and floating windows
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "*",
+  callback = function()
+    vim.api.nvim_set_hl(0, "Pmenu", { bg = "#2d2d2d" })
+    vim.api.nvim_set_hl(0, "PmenuSel", { bg = "#4a4a4a", fg = "#ffffff" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#2d2d2d" })
+    vim.api.nvim_set_hl(0, "FloatBorder", { bg = "#2d2d2d", fg = "#6a6a6a" })
+  end,
+})
