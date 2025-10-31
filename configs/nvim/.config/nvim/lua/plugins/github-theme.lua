7e263a6dc1d3347f60b8f840f05ee33dff5482ed
Amr Saber <amr.m.saber.mail@gmail.com>
2025-10-31 03:46:18 +0000

    nvim: update theme setup

--- configs/nvim/.config/nvim/lua/plugins/github-theme.lua
+++ configs/nvim/.config/nvim/lua/plugins/github-theme.lua
@@ -1,15 +1,23 @@
 return {
-  'projekt0n/github-nvim-theme',
-  name = 'github-theme',
-  lazy = false,    -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other start plugins
-  config = function()
-    require('github-theme').setup({
-      options = {
-        transparent = true
-      }
-    })
+  {
+    "projekt0n/github-nvim-theme",
+    name = "github-theme",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require("github-theme").setup({
+        options = {
+          transparent = true,
+        },
+      })
+    end,
+  },
 
-    vim.cmd('colorscheme github_dark')
-  end,
+  -- Configure lazyvim to load github theme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "github_dark",
+    },
+  },
 }
