61a4a590f622d8769efb0180777e09d270d2fd00
Amr Saber <amrsaber@amazon.com>
2025-11-11 11:22:58 +0000

    nvim: disable treesitter for AL2

--- configs/nvim/.config/nvim/lua/plugins/disabled.lua
+++ configs/nvim/.config/nvim/lua/plugins/disabled.lua
@@ -1,3 +1,10 @@
 return {
   { "folke/noice.nvim", enabled = false },
+  {
+    "nvim-treesitter/nvim-treesitter",
+
+    -- Disable treesitter on AL2
+    enabled = not vim.fn.filereadable("/etc/system-release-cpe") or
+              not vim.fn.readfile("/etc/system-release-cpe")[1]:match("amazon_linux:2"),
+  },
 }
