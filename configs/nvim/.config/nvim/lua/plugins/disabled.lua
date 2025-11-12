547f9d2d313d61552715bc5e6b1d17e12a0e98f4
Amr Saber <amr.m.saber.mail@gmail.com>
2025-11-12 01:23:52 +0000

    nvim: disable treesitter-textobjects with treesitter

--- configs/nvim/.config/nvim/lua/plugins/disabled.lua
+++ configs/nvim/.config/nvim/lua/plugins/disabled.lua
@@ -1,8 +1,12 @@
 local system = require("config.system")
 
+-- Disable treesitter on AL2
+local enable_treesitter = not system.is_al2()
+
 return {
+  -- Permanently disable noice
   { "folke/noice.nvim", enabled = false },
 
-  -- Disable treesitter on AL2
-  { "nvim-treesitter/nvim-treesitter", enabled = not system.is_al2() },
+  { "nvim-treesitter/nvim-treesitter", enabled = enable_treesitter },
+  { "nvim-treesitter/nvim-treesitter-textobjects", enabled = enable_treesitter },
 }
