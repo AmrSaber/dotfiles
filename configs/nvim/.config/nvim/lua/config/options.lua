589dbd40e81cbba70c593f3c48e71c0712b0f3c1
Amr Saber <amr.m.saber.mail@gmail.com>
2025-11-14 20:28:14 +0000

    nvim: add rounded window border

--- configs/nvim/.config/nvim/lua/config/options.lua
+++ configs/nvim/.config/nvim/lua/config/options.lua
@@ -31,12 +31,13 @@ vim.opt.conceallevel = 0
 vim.g.trouble_lualine = false
 
 -- Override colors for popups and floating windows
+vim.opt.winborder = "rounded"
 vim.api.nvim_create_autocmd("ColorScheme", {
   pattern = "*",
   callback = function()
     vim.api.nvim_set_hl(0, "Pmenu", { bg = "#2d2d2d" })
-    vim.api.nvim_set_hl(0, "PmenuSel", { bg = "#4a4a4a", fg = "#ffffff" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#2d2d2d" })
     vim.api.nvim_set_hl(0, "FloatBorder", { bg = "#2d2d2d", fg = "#6a6a6a" })
+    vim.api.nvim_set_hl(0, "PmenuSel", { bg = "#4a4a4a", fg = "#ffffff" })
   end,
 })
