94d143a1ff40c3522a1f447bd8a4960691b8badd
Amr Saber <amr.m.saber.mail@gmail.com>
2026-03-01 19:46:37 +0000

    nvim: update completion behaviour
    - Nothing is selected by default
    - Nothing is inserted with Enter unless something is selected

--- configs/nvim/.config/nvim/lua/plugins/cmp.lua
+++ configs/nvim/.config/nvim/lua/plugins/cmp.lua
@@ -1,26 +1,40 @@
 return {
   "hrsh7th/nvim-cmp",
-  opts = {
-    completion = {
-      completeopt = "menu,menuone,noinsert",
-    },
+  opts = function(_, opts)
+    local cmp = require("cmp")
+
+    opts.completion = {
+      completeopt = "menu,menuone,noselect",
+    }
 
     -- Only use built-in snippets, do not insert from LSP
-    snippet = {
+    opts.snippet = {
       expand = function(args)
         vim.snippet.expand(args.body)
       end,
-    },
+    }
 
-    sources = {
+    opts.sources = {
       { name = "nvim_lsp" }, -- LSP
       { name = "path" }, -- file system path
-    },
+    }
 
-    performance = {
+    opts.performance = {
       debounce = 500, -- ms to wait after last keystroke
       throttle = 500, -- ms between successive completion requests
       fetching_timeout = 500, -- optional: cancel slow sources
-    },
-  },
+    }
+
+    opts.mapping = vim.tbl_extend("force", opts.mapping or {}, {
+      ["<CR>"] = cmp.mapping(function(fallback)
+        if cmp.get_selected_entry() then
+          cmp.confirm({ select = false })
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+    })
+
+    return opts
+  end,
 }
