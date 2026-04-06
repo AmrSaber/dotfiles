d350df2622c1134c3f8f91da54af2a6dc8e2d094
Amr Saber <amr.m.saber.mail@gmail.com>
2026-04-06 03:15:40 +0100

    Disable unused themes

--- configs/nvim/.config/nvim/lua/plugins/disabled.lua
+++ configs/nvim/.config/nvim/lua/plugins/disabled.lua
@@ -4,8 +4,10 @@ local system = require("config.system")
 local enable_treesitter = not system.is_al2()
 
 return {
-  -- Permanently disable noice
+  -- Permanently disabled
   { "folke/noice.nvim", enabled = false },
+  { "catppuccin/nvim", enabled = false },
+  { "folke/tokyonight.nvim", enabled = false },
 
   { "nvim-treesitter/nvim-treesitter", enabled = enable_treesitter },
   { "nvim-treesitter/nvim-treesitter-textobjects", enabled = enable_treesitter },
