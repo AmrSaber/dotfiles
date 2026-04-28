36457a4471862b170099d56cfcce6acbb19a34a1
Amr Saber <amrsaber@amazon.com>
2026-04-28 16:20:07 +0000

    nvim: esc to close hover pop-up

--- configs/nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ configs/nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -20,5 +20,30 @@ return {
     -- This allows SQL injection highlighting in strings to work
     vim.highlight.priorities.semantic_tokens = 95
     vim.highlight.priorities.treesitter = 100
+
+    -- Make <Esc> close the LSP hover float from the source buffer.
+    -- Wrapping open_floating_preview is necessary because vim.lsp.buf.hover() in
+    -- 0.11+ no longer exposes the float's bufnr/winnr (no callback, no global handler).
+    local orig = vim.lsp.util.open_floating_preview
+    vim.lsp.util.open_floating_preview = function(contents, syntax, opts)
+      local src_buf = vim.api.nvim_get_current_buf()
+      local bufnr, winnr = orig(contents, syntax, opts)
+      if opts and opts.focus_id == "textDocument/hover" and winnr and vim.api.nvim_win_is_valid(winnr) then
+        vim.keymap.set("n", "<Esc>", function()
+          if vim.api.nvim_win_is_valid(winnr) then
+            vim.api.nvim_win_close(winnr, true)
+          end
+          pcall(vim.keymap.del, "n", "<Esc>", { buffer = src_buf })
+        end, { buffer = src_buf, silent = true, nowait = true, desc = "Close hover" })
+        vim.api.nvim_create_autocmd("WinClosed", {
+          pattern = tostring(winnr),
+          once = true,
+          callback = function()
+            pcall(vim.keymap.del, "n", "<Esc>", { buffer = src_buf })
+          end,
+        })
+      end
+      return bufnr, winnr
+    end
   end,
 }
